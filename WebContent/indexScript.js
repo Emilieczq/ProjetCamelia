0e8802d8c938aaba9170cae45c09ba241d5aa87e
Emilieczq <emilieczq@gmail.com>
2017-11-28 12:40:51 +0100

    create some js jsp html file

---
+++ WebContent/indexScript.js
@@ -0,0 +1,13 @@
+/**
+ * transfer login and password to servlet and check it then return the message
+ */
+
+function validate() {
+	$.post("UserServlet",{login:$("#login").val(),password:$("#password").val()}, function(data,status) {
+		$("#message").text(data);
+	});
+}
+
+
+
+
--- src/UserServlet.java
+++ src/UserServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,27 +16,27 @@ import javax.servlet.http.HttpServletResponse;
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public UserServlet() {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		/**
+		 * check login and password
+		 * connect to database
+		 */
+		
+//		if(login.equals("abc")&&password.equals("123")) {
+//			out.print("1");
+//		}else {
+//			out.print("0");
+//		}
 	}
 
 }
