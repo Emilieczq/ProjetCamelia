75f25c3702b0b51e121a594aed2f1b8f9e585340
Emilieczq <emilieczq@gmail.com>
2017-12-16 14:31:49 +0100

    replace index.html of index.jsp

---
+++ WebContent/index.jsp
@@ -0,0 +1,51 @@
+<%@page import="java.util.Iterator"%>  
+<%@page import="java.util.List"%>  
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="indexStyle.css">
+<title>ISEP Conseil - Login</title>
+</head>
+<body>
+<img src="pictures/login3.jpg" border='0'width='100%' height='100%' style='position: absolute;left:0px;top:0px;z-index: -1'/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="indexScript.js" type="text/javascript"></script>
+
+
+<div class= "center">
+<p class="title">Bienvenue<p>
+<%  
+String info= (String) request.getAttribute("info");  
+if(info!=null){  
+     
+%>  
+<h4 style='color:red'><%=info%></h4>  
+<%    
+}  
+%> 
+	<form name="loginForm" method= "POST" action="UserServlet">
+		<fieldset>
+			<label for="login">Nom d'utilisateur:</label>
+			<input id="login" type="text" name="login" />
+			<br><br>
+			<label for="password">Mot de passe:</label>
+		    <input id="password" type="password" name="password" />
+			<br><br>
+	    </fieldset>
+	    
+	    <input id="submit" type="submit" value="Connexion" class = "button"/>
+	  <input type="button" value="Inscription" class = "button" onclick="location.href='http://localhost:8080/ProjetCamelia/Inscription.html'" />
+	    <br>
+	</form>
+	<h3><a href="http://localhost:8080/ProjetCamelia/forget.jsp">Mot de passe oublié</a></h3>
+</div>
+
+
+
+<div id="message"></div>
+<br>
+</body>
+</html>
--- src/fr/isepconseil/control/UserServlet.java
+++ src/fr/isepconseil/control/UserServlet.java
@@ -1,4 +1,5 @@
 package fr.isepconseil.control;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -24,60 +25,55 @@ import fr.isepconseil.vo.User;
 @WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; // need to download MySQL JDBC jar
-    static final String DB_URL = "jdbc:mysql://localhost:3306/Camelia"; // correct the project name
 
-    static final String USER = "root"; // your server user name
-    static final String PASSWORD = "123456"; // your password
-    
-    public UserServlet() {
-        super();
-    }
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; // need to download MySQL JDBC jar
+	static final String DB_URL = "jdbc:mysql://localhost:3306/Camelia"; // correct the project name
+
+	static final String USER = "root"; // your server user name
+	static final String PASSWORD = "123456"; // your password
+
+	public UserServlet() {
+		super();
+	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String login = request.getParameter("login"); // login => email
 		String password = request.getParameter("password");
 		response.setContentType("text/html");
-		
-	    List<String> info=new ArrayList<String>();  
-	    if(login==null||"".equals(login)){ 
-	        info.add("Login ne peut pas être vide !");  
-	        System.out.println("Login ne peut pas être vide !");  
-	    }  
-	  
-	    if(password==null||"".equals(password)){ 
-	        info.add("Mot de passe ne peut pas être vide !");  
-	        System.out.println("Mot de passe ne peut pas être vide !");  
-	    }  
-	    if(info.size()==0){  
-	        User user=new User();  
-	        user.setEmail(login);  
-	        user.setPassword(password);  
-	        request.setAttribute("user", user);
-	        UserDAOProxy userDAOProxy=new UserDAOProxy(); 
-	        
-	        try {  
-	              
-	            if(userDAOProxy.findLogin(user)){  
-	                System.out.println("right");
-	                RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/recherchePourEleve.jsp");
-	                dispatcher.forward(request,response);
-	                
-	            }else {  
-	            		System.out.println("wrong");
-	            }                         
-	        } catch (Exception e) {  
-	            e.printStackTrace();  
-	        }  
-	    }  
-	    
-       
 
-		
-		
-		
-		
+		String info = null;
+		if ((login == null || "".equals(login))||(password == null || "".equals(password))) {
+			info = "La connexion a échoué, merci d'essayer de nouveau";
+			System.out.println("login ou passworld vide"); // to be deleted
+		}
+		if (info == null) {
+			User user = new User();
+			user.setEmail(login);
+			user.setPassword(password);
+			request.setAttribute("user", user);
+			UserDAOProxy userDAOProxy = new UserDAOProxy();
+
+			try {
+
+				if (userDAOProxy.findLogin(user)) {
+					System.out.println("right login and password"); // to be deleted
+					RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/recherchePourEleve.jsp");
+					dispatcher.forward(request, response);
+
+				} else {
+					System.out.println("wrong"); // to be deleted
+					info="La connexion a échoué, merci d'essayer de nouveau";
+					request.setAttribute("info", info);
+					RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+					dispatcher.forward(request, response);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			request.setAttribute("info", info);
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
-
 }
